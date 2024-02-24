3a2c3b4c848e6903a2c32eb599b3f31b8970f6fd
workRK <rk29work@proton.me>
2024-02-24 17:24:33 +0200

    Final project

--- src/main/webapp/html/script.js
+++ src/main/webapp/html/script.js
@@ -3,9 +3,22 @@ let accountsPerPage = 3;
 let accountsAmount = null;
 let currentPageNumber = 0;
 
+const RACE_ARRAY = ['HUMAN', 'DWARF', 'ELF', 'GIANT', 'ORC', 'TROLL', 'HOBBIT'];
+const PROFESSION_ARRAY = ['WARRIOR', 'ROGUE', 'SORCERER', 'CLERIC', 'PALADIN', 'NAZGUL', 'WARLOCK', 'DRUID'];
+const BANNED_ARRAY = ['true', 'false'];
+
+initCreateForm()
 createAccountPerPageDropDown()
 fillTable(currentPageNumber, accountsPerPage)
 updatePlayersCount()
+
+function initCreateForm() {
+    const $raceSelect = document.querySelector('[data-create-race]');
+    const $professionSelect = document.querySelector('[data-create-profession]');
+
+    $raceSelect.insertAdjacentHTML('afterbegin', createSelectOptions(RACE_ARRAY, RACE_ARRAY[0]))
+    $professionSelect.insertAdjacentHTML('afterbegin', createSelectOptions(PROFESSION_ARRAY, PROFESSION_ARRAY[0]))
+}
 
 function fillTable(pageNumber, pageSize) {
     $.get(`http://localhost:8080/rest/players?pageNumber=${pageNumber}&pageSize=${pageSize}`, (players) => {
@@ -18,13 +31,13 @@ function fillTable(pageNumber, pageSize) {
             htmlRows +=
                 `<tr class="row" data-account-id="${player.id}">
                     <td class="cell cell-small">${player.id}</td>
-                    <td class="cell">${player.name}</td>
-                    <td class="cell">${player.title}</td>
-                    <td class="cell">${player.race}</td>
-                    <td class="cell">${player.profession}</td>
-                    <td class="cell">${player.level}</td>
-                    <td class="cell">${player.birthday}</td>
-                    <td class="cell">${player.banned}</td>
+                    <td class="cell" data-account-name>${player.name}</td>
+                    <td class="cell" data-account-title>${player.title}</td>
+                    <td class="cell" data-account-race>${player.race}</td>
+                    <td class="cell" data-account-profession>${player.profession}</td>
+                    <td class="cell" data-account-level>${player.level}</td>
+                    <td class="cell" data-account-birthday>${new Date(player.birthday).toLocaleDateString('uk')}</td>
+                    <td class="cell" data-account-banned>${player.banned}</td>
                     <td class="cell cell-auto">
                         <button class="edit-button" value="${player.id}"><img class="edit-image" src="../img/edit.png" alt="edit"></button>
                     </td>
@@ -115,6 +128,30 @@ function setActiveButton(buttonIndex = 0) {
     $targetButton.classList.add('active-pagination-button');
 }
 
+function createAccount() {
+    const data = {
+        name: $('[data-create-name]').val(),
+        title: $('[data-create-title]').val(),
+        race: $('[data-create-race]').val(),
+        profession: $('[data-create-profession]').val(),
+        level: $('[data-create-level]').val(),
+        birthday: new Date($('[data-create-birthday]').val()).getTime(),
+        banned: $('[data-create-banned]').val() === 'on',
+    }
+
+    $.ajax({
+        url: `/rest/players/`,
+        type: 'POST',
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json",
+        success: function () {
+            updatePlayersCount();
+            fillTable(currentPageNumber, accountsPerPage);
+        }
+    })
+}
+
 function removeAccountHandler(e) {
     const accountId = e.currentTarget.value;
 
@@ -125,15 +162,87 @@ function removeAccountHandler(e) {
             updatePlayersCount();
             fillTable(currentPageNumber, accountsPerPage);
         }
-    });
+    })
+}
+
+function updateAccount({accountId, data}) {
+    $.ajax({
+        url: `/rest/players/${accountId}`,
+        type: 'POST',
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json",
+        success: function () {
+            updatePlayersCount();
+            fillTable(currentPageNumber, accountsPerPage);
+        }
+    })
 }
 
 function editAccountHandler(e) {
     const accountId = e.currentTarget.value;
-    console.log("edit")
 
-    const currentRow = document.querySelector(`.row[data-account-id='${accountId}']`);
 
-    const currentImage = currentRow.querySelector('.edit-button img');
-    currentImage.src = "../img/save.png";
+    const $currentRow = document.querySelector(`.row[data-account-id='${accountId}']`);
+    const $currentDeleteButton = $currentRow.querySelector('.delete-button');
+    const $currentImage = $currentRow.querySelector('.edit-button img');
+
+    const $currentName = $currentRow.querySelector('[data-account-name]');
+    const $currentTitle = $currentRow.querySelector('[data-account-title]');
+    const $currentRace = $currentRow.querySelector('[data-account-race]');
+    const $currentProfession = $currentRow.querySelector('[data-account-profession]');
+    const $currentBanned = $currentRow.querySelector('[data-account-banned]');
+
+    $currentImage.src = "../img/save.png";
+
+    $currentImage.addEventListener('click', () => {
+        const params = {
+            accountId: accountId,
+            data: {
+                name: $currentName.childNodes[0].getAttribute('data-value'),
+                title: $currentTitle.childNodes[0].getAttribute('data-value'),
+                race: $currentRace.childNodes[0].getAttribute('data-value'),
+                profession: $currentProfession.childNodes[0].getAttribute('data-value'),
+                banned: $currentBanned.childNodes[0].getAttribute('data-value')
+            }
+        }
+        updateAccount(params)
+    })
+
+    $currentName.childNodes[0].replaceWith(createInput($currentName.innerHTML))
+    $currentTitle.childNodes[0].replaceWith(createInput($currentTitle.innerHTML))
+    $currentRace.childNodes[0].replaceWith(createSelect(RACE_ARRAY, $currentRace.innerHTML))
+    $currentProfession.childNodes[0].replaceWith(createSelect(PROFESSION_ARRAY, $currentProfession.innerHTML))
+    $currentBanned.childNodes[0].replaceWith(createSelect(BANNED_ARRAY, $currentBanned.innerHTML))
 }
+
+function createInput(vale) {
+    const $htmlInputElement = document.createElement('input');
+
+    $htmlInputElement.setAttribute('type', 'text');
+    $htmlInputElement.setAttribute('value', vale);
+    $htmlInputElement.setAttribute('data-value', vale);
+
+    $htmlInputElement.addEventListener('input', e => {
+        $htmlInputElement.setAttribute('data-value', `${e.currentTarget.value}`)
+    })
+
+    return $htmlInputElement;
+}
+
+function createSelect(optionsArray, defaultValue) {
+    const $options = createSelectOptions(optionsArray, defaultValue);
+    const $selectElement = document.createElement('select');
+
+    $selectElement.insertAdjacentHTML('afterbegin', $options);
+
+    $selectElement.setAttribute('data-value', defaultValue);
+
+    $selectElement.addEventListener('change', e => {
+        $selectElement.setAttribute('data-value', e.currentTarget.value)
+    })
+
+    return $selectElement
+}
+
+
